3353a95c2ec48f134ebaea3fb88b93637983b146
aureliakdr <aureliakdr@gmail.com>
2026-06-15 16:01:55 +0900

    Add files via upload

---
+++ js/gallery-data.js
@@ -0,0 +1,11 @@
+const galleryData = [
+  {id:1,title:"콜라주 포스터",category:"graphic",categoryName:"그래픽 디자인",date:"2026.06.01",image:"images/gallery/photo1.jpg",desc:"Y2K 프루티거 에어로 테마 콜라주 포스터 제작."},
+  {id:2,title:"아이덴티티 포스터",category:"graphic",categoryName:"그래픽 디자인",date:"2026.06.02",image:"images/gallery/photo2.jpg",desc:"바비(Barbie) 테마 아이덴티티 포스터 제작."},
+  {id:3,title:"일러스트레이션 포스터",category:"graphic",categoryName:"그래픽 디자인",date:"2026.06.03",image:"images/gallery/photo3.jpg",desc:"레몬 테마 일러스트레이션 포스터 제작."},
+  {id:4,title:"브랜드와 콜라보레이션 일러스트레이션",category:"illustration",categoryName:"일러스트레이션",date:"2026.06.04",image:"images/gallery/photo4.jpg",desc:"공차(Gong cha)와의 콜라보레이션 일러스트레이션 포스터 제작."},
+  {id:5,title:"일러스테레이션 실습",category:"illustration",categoryName:"일러스트레이션",date:"2026.06.05",image:"images/gallery/photo5.jpg",desc:"일러스트레이터 작품 모작을 통한 일러스트레이션 실습."},
+  {id:6,title:"한글 타이포그래피 실습",category:"typo",categoryName:"타이포그래피",date:"2026.06.06",image:"images/gallery/photo6.jpg",desc:"키워드 기반 한글 타이포그래피 제작."},
+  {id:7,title:"브랜드 로고",category:"typo",categoryName:"타이포그래피",date:"2026.06.06",image:"images/gallery/photo7.jpg",desc:"베이커리 브랜드 로고 한글 타이포그래피 제작."},
+  {id:8,title:"노인복지관 어르신 사진 촬영",category:"photo",categoryName:"지역사회",date:"2026.06.06",image:"images/gallery/photo8.jpg",desc:"노인복지관 어르신 인물 사진 촬영."},
+  {id:9,title:"사진 액자 전달",category:"photo",categoryName:"지역사회",date:"2026.06.06",image:"images/gallery/photo9.jpg",desc:"어르신 사진 액자 전달 활동."}
+];
